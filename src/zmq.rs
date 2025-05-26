ba003c2441cb353b53569454a522d3b069b37d07
Riccardo Casatta <riccardo@casatta.it>
2025-05-26 15:31:38 +0200

    in zmq test correctly generate an address

--- src/zmq.rs
+++ src/zmq.rs
@@ -16,11 +16,10 @@ pub fn process_zmq_message(
     let mut iter = msg.iter().map(|item| item.as_ref());
     let topic = iter.next().unwrap();
     let tx = iter.next().unwrap();
+    // TODO check there are no other parts of the message
     if topic == b"rawtx" {
         let tx = elements::Transaction::consensus_decode(tx)?;
-        println!("Received rawtx: {:?}", tx.txid());
         for out in tx.output {
-            println!("Output : {:?}", out);
             if let Some(addr) = elements::Address::from_script(
                 &out.script_pubkey,
                 None,
@@ -67,16 +66,26 @@ pub async fn start_zmq_listener(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use elements::hashes::{hash160, Hash};
     use elements::{encode::Encodable, Script};
-    use std::str::FromStr;
 
     #[test]
     fn test_process_zmq_message() {
-        let script_pubkey = Script::from_str("522102ebc62c20f1e09e169a88745f60f6dac878c92db5c7ed78c6703d2d0426a01f942102c2d59d677122bc292048833003fd5cb19d27d32896b1d0feec654c291f7ede9e52ae").unwrap();
+        // Create a P2PKH script (pay to public key hash)
+        let hash_bytes = [
+            0x16u8, 0xe1, 0xae, 0x70, 0xff, 0x0f, 0xa1, 0x02, 0x90, 0x5d, 0x4a, 0xf2, 0x97, 0xf6,
+            0x91, 0x2b, 0xda, 0x6c, 0xce, 0x19,
+        ];
+        let hash = hash160::Hash::from_slice(&hash_bytes).unwrap();
+        let p2pkh_script = Script::new_p2pkh(&elements::PubkeyHash::from_raw_hash(hash));
+
         let tx_hex = "0200000001010000000000000000000000000000000000000000000000000000000000000000ffffffff0401650101ffffffff020125b251070e29ca19043cf33ccd7324e2ddab03ecc4ae0b5e77c4fc0e5cf6c95a01000000000000000000016a0125b251070e29ca19043cf33ccd7324e2ddab03ecc4ae0b5e77c4fc0e5cf6c95a01000000000000000000266a24aa21a9ed94f15ed3a62165e4a0b99699cc28b48e19cb5bc1b1f47155db62d63f1e047d45000000000000012000000000000000000000000000000000000000000000000000000000000000000000000000";
         // Create a simple Elements transaction
-        let tx =
+        let mut tx =
             elements::Transaction::consensus_decode(&hex::decode(tx_hex).unwrap()[..]).unwrap();
+
+        // Replace the script_pubkey with our P2PKH script
+        tx.output[0].script_pubkey = p2pkh_script;
 
         // Serialize the transaction
         let mut tx_bytes = Vec::new();
